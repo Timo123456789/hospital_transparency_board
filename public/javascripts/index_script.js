8e12525e864a035fb316dd4c33db8adabe60f486
luening <he-luening@t-online.de>
2023-11-27 11:33:38 +0100

    added scale control

--- public/javascripts/index_script.js
+++ public/javascripts/index_script.js
@@ -6,6 +6,12 @@ L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{
 	attribution: '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
 	ext: 'png'
 }).addTo(map);
+
+// controls
 L.control.zoom({
     position: 'bottomright'
+}).addTo(map);
+L.control.scale({
+	metric: true,
+	position: 'bottomright'
 }).addTo(map);
