fb9594331cd445ac8d78efc71862496865bd9b66
luening <he-luening@t-online.de>
2023-12-04 17:17:56 +0100

    refactor

--- routes/file_route.js
+++ routes/file_route.js
@@ -7,9 +7,5 @@ const path = require('path');
 router.get('/', function(req, res, next) {
     res.sendFile(path.join(__dirname, '../data/krankenhausverzeichnis-Standorte.geojson'));
   });
-
-
-
-
-
+  
 module.exports = router;
