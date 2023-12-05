93456e9b577dd0b4f31253bb1011117af89a82d3
Timo Lietmeyer <timolietmeyer@googlemail.com>
2023-12-05 11:08:02 +0100

    add main function  to index script js

--- public/javascripts/index_script.js
+++ public/javascripts/index_script.js
@@ -1,3 +1,4 @@
+
 var map = L.map("map", { zoomControl: false }).setView([51.505, -0.09], 13);
 map.panTo(new L.LatLng(51.9607, 7.6261));
 L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
@@ -16,88 +17,51 @@ L.control.scale({
 }).addTo(map);
 
 
-//test
+main();
+function main() {
 
-/**
-*@desc declaration of Green Marker for Leafleat Map
-*@Source  https://github.com/pointhi/leaflet-color-markers/tree/master/img
-*/
-var greenIcon = new L.Icon({
-	iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png',
-	shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
-	iconSize: [25, 41],
-	iconAnchor: [12, 41],
-	popupAnchor: [1, -34],
-	shadowSize: [41, 41]
-});
+	/**
+	*@desc declaration of Green Marker for Leafleat Map
+	*@Source  https://github.com/pointhi/leaflet-color-markers/tree/master/img
+	*/
+	var greenIcon = new L.Icon({
+		iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png',
+		shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+		iconSize: [25, 41],
+		iconAnchor: [12, 41],
+		popupAnchor: [1, -34],
+		shadowSize: [41, 41]
+	});
 
-var hospIcon = new L.Icon({
-	iconUrl: 'https://icons.veryicon.com/png/o/healthcate-medical/medical-icon-library/hospital-9.png',
-	
-	iconSize: [25, 25],
-	iconAnchor: [6, 20],
-	popupAnchor: [5, -10],
-	shadowSize: [41, 41]
-});
-
-
-
-var data = JSON.stringify()
-var user_loc_marker = L.marker([51.9607, 7.6261], { icon: greenIcon })
-user_loc_marker.bindPopup("Your Position");
-map.addControl(user_loc_marker)
+	var hospIcon = new L.Icon({
+		iconUrl: 'https://icons.veryicon.com/png/o/healthcate-medical/medical-icon-library/hospital-9.png',
+		
+		iconSize: [25, 25],
+		iconAnchor: [6, 20],
+		popupAnchor: [5, -10],
+		shadowSize: [41, 41]
+	});
+	icons = [greenIcon, hospIcon]
+	set_user_marker([51.9607, 7.6261], icons)
+	set_kh_marker(10000, [51.9607, 7.6261], icons)
 
 
+}
 
 
-// URL der Datei
-var url = "http://localhost:3000/kh_verzeichnis";
-var markers_kh_pos = new Array();
-
-// Datei lesen
-fetch(url)
-	.then(response => response.text())
-	.then(data => {
-		// Verarbeiten Sie die Daten hier
-		data = JSON.parse(data);
-
-		// Angenommen, center ist der Mittelpunkt Ihres Radius
-		var center = L.latLng(51.9607, 7.6261);
-
-		// Angenommen, radius ist der Radius in Metern
-		var radius = 10000;
-
-		for (let i = 0; i < data.features.length; i++) {
-			var coords = [data.features[i].geometry.coordinates[1], data.features[i].geometry.coordinates[0]];
-
-			if (typeof coords[0] !== 'undefined' && typeof coords[1] !== 'undefined') {
-				// Berechnen Sie die Entfernung zwischen dem Mittelpunkt und dem Marker
-				var distance = center.distanceTo(coords);
-
-				if (distance <= radius) {
-					text_json = {
-						"Name": data.features[i].properties.USER_Adresse_Name_Standort,
-						"Strasse": data.features[i].properties.USER_Adresse_Strasse_Standort,
-						"HNR": data.features[i].properties.USER_Adresse_Haus_Nr__Standort,
-						"Postleitzahl": data.features[i].properties.USER_Adresse_Postleitzahl_Standort,
-						"Ort": data.features[i].properties.USER_Adresse_Ort_Standort
-					}
-					temp_marker = L.marker(coords, { icon: hospIcon }).bindPopup("Name: " + text_json.Name + "<br>" + "Adress: " + text_json.Strasse + " " + text_json.HNR + ", " + text_json.Postleitzahl + " " + text_json.Ort)
-					markers_kh_pos.push(temp_marker)
 
 
-					//markers_kh_pos = L.marker(coords).bindPopup("Name: " + text_json.Name + "<br>" + "Adress: " + text_json.Strasse +" " + text_json.HNR + ", " + text_json.Postleitzahl + " " + text_json.Ort).addTo(map);
 
-					// marker_group_kh_locs = L.featureGroup().addTo(map);
-					// L.marker(coords).addTo(marker_group_kh_locs).bindPopup("Name: " + text_json.Name + "<br>" + "Adress: " + text_json.Strasse +" " + text_json.HNR + ", " + text_json.Postleitzahl + " " + text_json.Ort);
-				}
-			}
-		}
-		for (let i = 0; i < markers_kh_pos.length; i++) {
-			map.addControl(markers_kh_pos[i])
-		}
-	})
-	.catch(error => console.error('Fehler beim Lesen der Datei:', error));
+
+
+
+
+// //test
+
+
+
+
+
 
 
 
@@ -120,7 +84,7 @@ document.getElementById('button_getUserLoc').addEventListener('click', function 
 
 			//Zoom to user location
 			//var markerBounds = L.latLngBounds(coords);
-			map.flyto(coords, 15);
+			map.flyTo(coords, 13);
 
 		}, function (error) {
 			console.error('Fehler beim Abrufen der Position:', error);
@@ -145,13 +109,15 @@ function clear_markers() {
 	user_loc_marker.remove()
 }
 
-function set_user_marker(coords) {
+function set_user_marker(coords, icons) {
+	var greenIcon = icons[0]
 	user_loc_marker = L.marker(coords, { icon: greenIcon })
 	user_loc_marker.bindPopup("Your Position");
 	map.addControl(user_loc_marker)
 }
 
-function set_kh_marker(radius, center) {
+function set_kh_marker(radius, center, icons) {
+	var hospIcon = icons[1]
 	var url = "http://localhost:3000/kh_verzeichnis";
 	var markers_kh_pos = new Array();
 	center = L.latLng(center[0], center[1]);
@@ -203,16 +169,4 @@ document.getElementById('button_PZC').addEventListener('click', function () {
 		
 
 
-// document.getElementById('button_PZC').addEventListener('click', function () {
-// 	console.log("button_PZC clicked");
-// 	var inputValue = document.getElementById('input_PZC').value;
-// 	console.log(inputValue);
-// 	// Hier können Sie Ihre Logik implementieren
-// 	var temp = geocoder.geocode(inputValue)
-		
-// 			console.log(inputValue);
-// 			console.log(res[0].latitude, res[0].longitude);
-
-
-// });
 	
