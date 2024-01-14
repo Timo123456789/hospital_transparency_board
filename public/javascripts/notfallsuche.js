0de43a0636d98eac1c9618cf4a6ee555bc3adeaa
luening <he-luening@t-online.de>
2024-01-14 16:52:11 +0100

    added notfallsuche

---
+++ public/javascripts/notfallsuche.js
@@ -0,0 +1,153 @@
+/**
+ * fetches data from files
+ * @returns hospitals
+ */
+async function getData () {
+  const url = 'http://localhost:3000/kh_verzeichnis'
+  try {
+    const response = await fetch(url)
+    const data = await response.text()
+    return JSON.parse(data)
+  } catch (error) {
+    console.error('Fehler beim Lesen der Datei:', error)
+  }
+}
+
+/**
+ * defines query and search after radio button selection
+ * @returns query and search
+ */
+function getQueryString () {
+  let query = ''
+  let search = ''
+  // Allgemeine stationäre Notfallversorgung
+  if (document.getElementById('btnradio1').checked) {
+    query = 'Allgemeine_Notfallversorgung'
+    search = 1
+  } else if (document.getElementById('btnradio2').checked) {
+    // Allgemeine stationäre Notfallversorgung
+    query = 'Spezielle_Notfallversorgung_schwerverletzte'
+    search = 1
+  } else if (document.getElementById('btnradio3').checked) {
+    // Allgemeine stationäre Notfallversorgung
+    query = 'Spezielle_Notfallversorgung_kinder'
+    search = 1
+  } else if (document.getElementById('btnradio4').checked) {
+    // Allgemeine stationäre Notfallversorgung
+    query = 'Spezielle_Notfallversorgung_schlaganfall'
+    search = 1
+  } else if (document.getElementById('btnradio5').checked) {
+    // Allgemeine stationäre Notfallversorgung
+    query = 'Spezielle_Notfallversorgung_Herz'
+    search = 1
+  }
+  return { query, search }
+}
+
+/**
+ * queries data
+ * @param {string} query
+ * @param {string} search
+ * @returns hospitals
+ */
+async function queryData (query, search, coords) {
+  const data = await getData()
+  const hospitals = data.features.filter(hospital => {
+    return hospital.properties[query] === search
+  })
+  // order the results after the distance
+  const orderedHospitals = orderHospitals(hospitals, coords)
+  // return the first 10 results
+  return orderedHospitals.slice(0, 10)
+}
+
+/**
+ * orders hospitals after distance
+ * @param {*} hospitals
+ * @param {*} coords
+ * @returns
+ */
+function orderHospitals (hospitals, coords) {
+  const orderedHospitals = hospitals.map(hospital => {
+    console.log(coords)
+    const distance = getDistanceFromLatLonInKm(coords.lat, coords.lon, hospital.geometry.coordinates[1], hospital.geometry.coordinates[0])
+    console.log(distance)
+    hospital.distance = distance
+    return hospital
+  })
+  orderedHospitals.sort((a, b) => {
+    return a.distance - b.distance
+  })
+  return orderedHospitals
+}
+
+/**
+ * calculates distance between two coordinates
+ * @param {*} lat1
+ * @param {*} lon1
+ * @param {*} lat2
+ * @param {*} lon2
+ * @returns distance in km
+ */
+function getDistanceFromLatLonInKm (lat1, lon1, lat2, lon2) {
+  const R = 6371 // Radius of the earth in km
+  const dLat = deg2rad(lat2 - lat1)
+  const dLon = deg2rad(lon2 - lon1)
+  const a =
+    Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+    Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) *
+    Math.sin(dLon / 2) * Math.sin(dLon / 2)
+
+  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a))
+  const d = R * c // Distance in km
+  return d
+}
+
+/**
+ * converts degree to radian
+ * @param {*} deg
+ * @returns radian
+ */
+function deg2rad (deg) {
+  return deg * (Math.PI / 180)
+}
+
+function writeTable (result) {
+  const notfallsucheTable = document.getElementById('notfallsucheTable')
+  notfallsucheTable.innerHTML = ''
+  const table = document.createElement('table')
+  const tbody = document.createElement('tbody')
+  result.forEach(hospital => {
+    const row = document.createElement('tr')
+    // name
+    const cell = document.createElement('td')
+    const cellText = document.createTextNode(hospital.properties.Adresse_Name_Standort)
+    cell.appendChild(cellText)
+    // distance
+    const cellText2 = document.createTextNode(hospital.distance.toFixed(2) + ' km')
+    cell.appendChild(cellText2)
+    // add cells to row
+    row.appendChild(cell)
+    tbody.appendChild(row)
+  })
+  table.appendChild(tbody)
+  notfallsucheTable.appendChild(table)
+  notfallsucheTable.setAttribute('border', '2')
+}
+
+// -------------------- Event Listener --------------------
+/**
+ * Event Listener for radio buttons
+ */
+document.getElementById('notfallsuche').addEventListener('change', async function () {
+  const { query, search } = getQueryString()
+  const userLocation = document.getElementById('userLocationField').value
+  if (userLocation === '') {
+    alert('Bitte lassen Sie Ihren Standort ermitteln.')
+    return
+  }
+  const userLocationParsed = JSON.parse(userLocation)
+  const result = await queryData(query, search, userLocationParsed)
+  console.log(result)
+  writeTable(result)
+})
